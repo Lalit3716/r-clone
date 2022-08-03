3e6b51e1627155e66451a1be7387933cec4d4c07
Lalit <lalitkumarsingh3716@gmail.com>
2022-08-03 22:23:50 +0530

    Enabled Absolute Imports

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import { GOOGLE_OAUTH } from "../../../keys";
+import { GOOGLE_OAUTH } from "keys";
 
 const prisma = new PrismaClient();
 
